70151520ce140fa7a607e8fa38204879f8f7cac7
Nikolay <n.delenyan@outlook.com>
2022-04-17 10:15:19 +0300

    20220417-684-done

--- Task684.cs
+++ Task684.cs
@@ -7,6 +7,7 @@ namespace euler_from26
     public static class Task684
     {
 
+        public static long mod = 1_000_000_007;
         public static int s(int n)
         {
             int i = 0;
@@ -24,12 +25,31 @@ namespace euler_from26
                 return (n % 9 + 1) * BigInteger.Pow(10, n / 9) - 1;
         }
 
-        public static BigInteger S(long k, BigInteger mod)
+        public static long S(long n, BigInteger mod)
         {
-            BigInteger sum = 5 * (BigInteger.Pow(10, k / 9) - 1);
-            for (int n = 1; n <= k; n++)
-                sum += s_fast(n);
-            return sum % mod;
+            var nd9 = n / 9;
+            var np9 = n % 9;
+            var pow = Math.Pow(10, nd9);
+            var k = (np9 + 1) * (np9 + 2) / 2;
+            var v = 5 * (pow - 1);
+            var w = pow * k;
+            return (long)(v + w - n - 1);
+        }
+
+        public static long S_mod(long n, long mod)
+        {
+            var nd9 = n / 9;
+            var np9 = n % 9;
+//            var pow = Math.Pow(10, nd9);
+            long pow = (long)BigInteger.ModPow(10, nd9, mod);
+            //            BigInteger nd9 = BigInteger.DivRem()
+            var k = (np9 + 1) * (np9 + 2) / 2;
+            var v = 5 * (pow - 1);
+            var w = pow * k;
+            long res = (long)((v + w - n - 1) % mod);
+            while(res < 0)
+                res += mod;
+            return res;
         }
 
         public static long[] F(long count)
@@ -44,14 +64,16 @@ namespace euler_from26
 
         public static void main()
         {
-            BigInteger mod = 1_000_000_007;
-//            System.Console.WriteLine(S(20, mod));
-            var f = F(90);
-            // System.Console.WriteLine(MyCollections.Print(f));
-            for (int i=2; i <= 90; i++)
-                Console.WriteLine($"{i}:\t{f[i]}:\t{S(f[i], mod)}");
-            // for (int i = 1; i < 20; i++)
-            //     System.Console.WriteLine($"{i}: {S(i, mod)}");
+            System.Console.WriteLine($"{S(20, mod)} {S_mod(20, mod)}");
+            var f = F(91);
+            long sum = 0;
+            for (int i = 2; i <= 90; i++)
+            {
+                sum += S_mod(f[i], mod);
+                sum = sum % mod;
+                Console.WriteLine($"{i}:\t{f[i]}:\t{S_mod(f[i], mod)}\t{sum}");
+            }
+            System.Console.WriteLine(sum);
         }
     }
 }
