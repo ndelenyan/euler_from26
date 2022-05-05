8f07d65f482bdcb223cabfc3cb17180e4d657d76
Nikolay Delenyan <n.delenyan@outlook.com>
2022-05-05 14:35:16 +0300

    20220505-357

--- Task357.cs
+++ Task357.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace euler_from26
 {
@@ -7,14 +9,30 @@ namespace euler_from26
 
         public static long[] primes;
 
+        public static IEnumerable<long> Prime_Divisors_All(long n)
+        {
+            yield return 1;
+            int index = 1;
+            while (n > 1)
+            {
+                while (n % primes[index] == 0)
+                {
+                    n /= primes[index];
+                    yield return primes[index];
+                }
+                index++;
+            }
+        }
+
+
         public static bool i(long candidate)
         {
-            foreach (var d in Prime.Divisors(candidate))
+            var d = MyCollections.toArray(Prime_Divisors_All(candidate));
+            foreach (var dd in Prime.combos(d))
             {
-                if (Array.BinarySearch(primes, d + candidate / d) < 0)
+                long ddd = (long)Functions.product(dd);
+                if (Array.BinarySearch(primes, ddd + candidate / ddd) < 0)
                     return false;
-                if (d >= candidate / d)
-                    return true;
             }
             return true;
         }
@@ -25,15 +43,23 @@ namespace euler_from26
             Console.Write("calculating primes... ");
             primes = Prime.Primes(100_000_000);
             Console.WriteLine("done");
-            for (int j = 0; j <= primes.Length; j++)
-            {
-                if (j % 10_000 == 0)
-                    Console.WriteLine($"{j}/{primes.Length}");
-                var candidate = primes[j] - 1;
-                if (i(candidate))
-                    sum += candidate;
-            }
-            Console.WriteLine(sum);
+            var d = MyCollections.toArray(Prime_Divisors_All(60));
+//            Console.WriteLine(MyCollections.Print(d));
+            foreach(var c in Prime.combos(d))
+                 Console.WriteLine($"{MyCollections.Print(c)} {Functions.product(c)}");
+            // int count = 0;
+            // Parallel.For(0, primes.Length, j =>
+            // {
+            //     count++;
+            //     if (count % 10_000 == 0)
+            //         Console.WriteLine($"{count}/{primes.Length}:{sum}");
+            //     var candidate = primes[j] - 1;
+            //     if ((candidate / 2) % 2 == 0)
+            //         return;
+            //     else if (i(candidate))
+            //             sum += candidate;
+            // });
+            // Console.WriteLine(sum);
         }
     }
 }
