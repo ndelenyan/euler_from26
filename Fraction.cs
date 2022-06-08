91c71199875c345f5e76cdf60c5c51796ed5bd05
Nikolay Delenyan <n.delenyan@outlook.com>
2022-06-08 14:43:37 +0300

    20220608-065-done

--- Fraction.cs
+++ Fraction.cs
@@ -9,10 +9,16 @@ namespace euler_from26
             nominator = 1;
             denominator = 1;
         }
-        public static Fraction operator +(long start, Fraction f) => new Fraction() {nominator = f.nominator + start * f.denominator, denominator = f.denominator};
-        public static Fraction operator +(Fraction f, Fraction g) => new Fraction() {nominator = f.nominator * g.denominator + g.nominator * f.denominator, denominator = f.denominator * g.denominator};
-        public static bool operator == (Fraction f, long i) => f.nominator / f.denominator == i && f.nominator % f.denominator == 0;
-        public static bool operator != (Fraction f, long i) => f.nominator / f.denominator != i || f.nominator % f.denominator != 0;
+        public Fraction(long n, long d)
+        {
+            nominator = n;
+            denominator = d;
+        }
+
+        public static Fraction operator +(long start, Fraction f) => new Fraction(f.nominator + start * f.denominator, f.denominator);
+        public static Fraction operator +(Fraction f, Fraction g) => new Fraction(f.nominator * g.denominator + g.nominator * f.denominator, f.denominator * g.denominator);
+        public static bool operator == (Fraction f, long i) => (f.nominator / f.denominator == i) && (f.nominator % f.denominator == 0);
+        public static bool operator != (Fraction f, long i) => (f.nominator / f.denominator != i) || (f.nominator % f.denominator != 0);
 
         public Fraction inverse() => new Fraction(){nominator = this.denominator, denominator = this.nominator};
 
--- Task065.cs
+++ Task065.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace euler_from26
 {
@@ -16,36 +17,83 @@ namespace euler_from26
             }
         }
 
-        public static Fraction convergent(long start, long[] gens)
+        public static long[] gen_e(long n)
         {
-            Fraction F = new Fraction();
-            Fraction R = new Fraction() { nominator = start, denominator = 1};
-            Array.Reverse(gens);
-            foreach(var gen in gens)
+            long[] res = new long[n];
+            res[0] = 1;
+            res[1] = 2;
+            long i = 2;
+            long count = 0;
+            long k = 2;
+            while(i < n)
             {
-                if (R == 1)
+                if (count == 2)
                 {
-                    R.nominator = gen;
-                    R.denominator = 1;
+                    res[i] = 2 * k;
+                    k++;
+                    count = 0;
                 }
                 else
                 {
-                    R = gen + R.inverse();
+                    res[i] = 1;
+                    count++;
                 }
-                Console.WriteLine(R);
+                i++;
             }
-            return start + R;
+            return res;
         }
+
+        public static Fraction convergent(long start, long[] gens)
+        {
+            Fraction R = new Fraction();
+            // Array.Reverse(gens);
+            for(int i = gens.Length - 1; i >= 0; i--)
+            {
+                if (i == gens.Length - 1)
+                    R = new Fraction(gens[i], 1);
+                else
+                    R = gens[i] + R.inverse();
+                // Console.WriteLine(R);
+            }
+            return start + R.inverse();
+        }
+
+        public static Fraction[] convergents(long start, long[] gens)
+        {
+            Fraction[] res = new Fraction[gens.Length + 1];
+            res[0] = new Fraction(start, 1);
+            long i = 1;
+            // foreach(var g in fabric(gens))
+            //     Console.WriteLine(MyCollections.Print(g));
+            foreach(var g in fabric(gens))
+            {
+                Console.Write($"{MyCollections.Print(g)}\t");
+                var z = convergent(start, g);
+                Console.WriteLine($"{z}");
+                res[i] = z;
+                i++;
+            }
+            return res;
+        }
+
         public static void main ()
         {
-            // var a = new long[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-            var a = new long[]{2, 2};
-            // foreach(var b in fabric(a))
-            //     Console.WriteLine(MyCollections.Print(b));
-            var b = convergent(1, a);
-            Console.WriteLine(b);
-            // foreach(var c in b)
-            //     Console.WriteLine(c);
+
+            var e = gen_e(100);
+            Console.WriteLine(MyCollections.Print(e));
+            var b = convergents(2, e);
+            BigInteger nnm2 = 2;
+            BigInteger nnm1 = 3;
+            BigInteger nn = 0;
+            for(int i = 2; i < b.Length - 1; i++)
+            {
+                nn = nnm2 + nnm1 * e[i-1];
+                Console.WriteLine($"{i+1}\t{b[i].nominator}\t{nn}\t{e[i]}");
+                nnm2 = nnm1;
+                nnm1 = nn;
+            }
+            Console.WriteLine(Digits.digit_sum(nn));
+            // 	
         }
     }
 }
