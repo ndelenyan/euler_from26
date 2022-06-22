f17bf4d238cb904100bf73d005b2c4fadc4a122e
Nikolay Delenyan <n.delenyan@outlook.com>
2022-06-22 19:00:35 +0300

    20220622-070-01

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace euler_from26
 
         static void Main(string[] args)
         {
-            Task072.main();
+            Task070.main();
         }
     }
 }
---
+++ Task070.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace euler_from26
+{
+    public static class Task070
+    {
+        public static void main ()
+        {
+            Console.WriteLine("Task070");
+        }
+    }
+}
