57e4d81bed66824aac349d25ef0ccf4e3977590e
Nikolay Delenyan <n.delenyan@outlook.com>
2022-07-01 17:52:22 +0300

    20220701-072-done

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace euler_from26
 
         static void Main(string[] args)
         {
-            Task070.main();
+            Task072.main();
         }
     }
 }
--- Task072.cs
+++ Task072.cs
@@ -13,38 +13,14 @@ namespace euler_from26
         public static void main ()
         {
             Console.WriteLine("Task072");
-            for(long i = 1; i <= max; i++)
+            long sum = 0;
+            for (long i = 2; i <= max; i++)
             {
-                if (i % 10_000 == 0)
-                    Console.WriteLine($"{i} / {max}");
-                var h = Prime.Prime_Unique_Divisors(i, primes, false);
-                divisors[i] = new long[h.Count];
-                int j = 0;
-                foreach(var c in h)
-                    divisors[i][j++] = c;
+                if (i%10_000 == 0)
+                    Console.WriteLine(i);
+                sum += Prime.totient(i, primes);
             }
-            divisors[1] = new long[] {1};
-            // for (int i = 1; i <= 10; i++)
-            //     Console.WriteLine(MyCollections.Print(divisors[i]));
-            long count = 0;
-            Parallel.For(1, max + 1, i =>
-            {
-                if (i % 10_000 == 0)
-                    Console.WriteLine($"{i} / {max}");
-                for(long j = 1; j < i; j++)
-                {
-                    bool are_reducible = false;
-                    foreach (var d in divisors[j])
-                        if (Array.BinarySearch(divisors[i], d) >= 0)
-                        {
-                            are_reducible = true;
-                            break;
-                        }
-                    if (! are_reducible)
-                        count++;                    
-                }
-            });
-            Console.WriteLine(count);
+            Console.WriteLine(sum);
         }
     }
 }
